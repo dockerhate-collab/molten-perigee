78fca1b133f915d635b620d9d6a38d15b99a7cf7
Cover Oregon <coveroregonhelp@gmail.com>
2025-12-06 15:06:16 -0800

    chore: Remove temporary debug files and scripts.

--- debug_system.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-whoami > debug_output.txt
-echo "---" >> debug_output.txt
-ls -la /etc/sddm* >> debug_output.txt 2>&1
-echo "---" >> debug_output.txt
-cat /etc/passwd | grep nnormal >> debug_output.txt
-echo "End of debug" >> debug_output.txt
